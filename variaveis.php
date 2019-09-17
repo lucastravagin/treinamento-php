0fde5f5d826bee41f441ec115144cb6d419e8a15
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-09-17 16:01:44 -0300

    Variaveis

---
+++ variaveis.php
@@ -0,0 +1,19 @@
+<?php 
+    echo "Isto é uma mensagem <br>";
+    print "Isto é uma mensagem com print <br>";
+
+    //Criação de variáveis com valor
+    
+    $variavel_com_valor = 10; //Variável do tipo inteiro
+
+
+    $variavel_texto = 'Variável do tipo String'; //Variável so tipo String
+
+    echo $variavel_texto . "<br>" .$variavel_com_valor;
+
+    $nome = 'Lucas';
+    $sobrenome = 'Travagin';
+    $ultimo_nome = 'Pereira';
+
+    echo "<br> $nome $sobrenome $ultimo_nome";
+?>
