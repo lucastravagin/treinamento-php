58552319b2056bd7a0d1a187f2f5c320d59c799f
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-09-17 16:12:41 -0300

    Variaveis pré definidas

---
+++ variaveis_pre_definidas.php
@@ -0,0 +1,19 @@
+<?php  
+    //Superglobais
+    //Disponíveis em todos os escopos para todo o Script
+    
+    /* Exemplos 
+        * $GLOBALS,
+        * $_SERVER,
+        * $_GET,
+        * $_POST,
+        * $_FILES,
+        * $_COOKIE,
+        * $_SESSION,
+        * $_REQUEST,
+        * $_ENV
+
+    */
+
+    
+?>
